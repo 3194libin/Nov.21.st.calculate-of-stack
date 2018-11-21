f192a8dcae98fd4290a9660077548a06968a76ba
libin0205 <1611774332@qq.com>
2018-11-21 09:21:46 +0800

    Initial Commit

---
+++ Nov.21.st.calculate of stack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nov.21.st.calculate of stack
+//
+//  Created by s20171103194 on 2018/11/21.
+//  Copyright © 2018 s20171103194. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Nov.21.st.calculate of stackTests/Nov_21_st_calculate_of_stackTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nov_21_st_calculate_of_stackTests.swift
+//  Nov.21.st.calculate of stackTests
+//
+//  Created by s20171103194 on 2018/11/21.
+//  Copyright © 2018 s20171103194. All rights reserved.
+//
+
+import XCTest
+@testable import Nov_21_st_calculate_of_stack
+
+class Nov_21_st_calculate_of_stackTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
