eda73b0399e7384afb98013b81543e90e5e21c68
libin0205 <1611774332@qq.com>
2019-01-05 08:51:58 +0800

    1.5

--- Nov.21.st.calculate of stack/ViewController.swift
+++ Nov.21.st.calculate of stack/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    class NumberStack{
+        var num = [Int](repeating:0,count:100)
+        var top = -1;
+        func InitNStack() {
+            self.top = -1;
+        }
+        func PushNStack(x:Int){
+            self.top =  self.top + 1
+            self.num[self.top]=x
+        }
+        func PopNStack(x:Int) -> Int {
+            
+        }
+    }
+    @IBOutlet weak var textbox: UITextField!
+    @IBAction func button1(_ sender: Any) {
+    }
+    @IBAction func button2(_ sender: Any) {
+    }
+    @IBAction func button3(_ sender: Any) {
+    }
+    @IBAction func Add(_ sender: Any) {
+    }
+    @IBAction func Prime(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
